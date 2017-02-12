bed8d281f6df54d0da684046f8dcfcf117a93ce6
Alexander Koltun <alexander.koltun@gmail.com>
2017-02-12 13:16:13 +0100

    Add one second delay to all server responses

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ var items = require('./data').items;
 application.use(cors());
 
 application.get('/', function(req, res) {
-  res.json(items);
+  setTimeout(() => res.json(items), 1000);
+});
+
+application.get('/posts/:id', function(req, res) {
+  var id = req.params.id;
+  setTimeout(() => res.json(items[id]), 1000);
+});
+
+application.post('/posts/:id/like', function(res, req) {
+  var id = req.params.id;
+  items[id].meta.likes = items[id].meta.likes + 1;
+  setTimeout(() => res.json(items[id]), 1000);
 });
 
 application.listen(3002, function() {
