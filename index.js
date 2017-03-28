da10644f8585b90ab2e6810ded711d07f97b8328
Alexander Koltun <alexander.koltun@gmail.com>
2017-03-28 23:15:23 +0200

    Add incoming param (id) validation

--- index.js
+++ index.js
@@ -10,17 +10,25 @@ const defaultDelay = 2000;
 const delayResponse = (response, delay = defaultDelay) =>
   setTimeout(response, delay);
 
+const validateId = id => 0 <= id && id < items.length;
+
 application.get('/', function(req, res) {
   delayResponse(() => res.json(items));
 });
 
 application.get('/posts/:id', function(req, res) {
   var id = req.params.id - 1;
+
+  if (!validateId(id)) return delayResponse(() => res.sendStatus(404));;
+
   delayResponse(() => res.json(items[id]));
 });
 
 application.post('/posts/:id/like', function(req, res) {
   var id = req.params.id - 1;
+
+  if (!validateId(id)) return delayResponse(() => res.sendStatus(404));;
+
   items[id].meta.likes.count = items[id].meta.likes.count + 1;
   delayResponse(() => res.json(items[id]));
 });
